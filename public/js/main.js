d5018ce18fae0ccca0efd0b1e87ed1ab264f359e
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-10-25 23:09:18 +0300

    added the messages to the DOM

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,10 @@ const chatForm = document.getElementById('chat-form');
 
 const socket = io();
 
+// Message from server
 socket.on('message', message => {
   console.log(message);
+  outputMessage(message);
 });
 
 // message submit
@@ -14,4 +16,15 @@ chatForm.addEventListener('submit', e => {
 
   // Emit message to server
   socket.emit('chatMessage', msg);
-})
+});
+
+
+// output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = ` <p class="meta">Mary <span>9:15pm</span></p>
+                    <p class="text"> ${message}</p>`
+
+  document.querySelector('.chat-messages').appendChild(div);
+}
